3cb989df57fbfeb9171565a5d0a73c9057bb53aa
jrizza88 <jetorres88@gmail.com>
2019-07-25 21:24:25 -0400

    set up react app client and necessary depencies

---
+++ expatjournal/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+        Hello!
+        <Button color="primary">ExPat!</Button>
+    </div>
+  );
+}
+
+export default App;
