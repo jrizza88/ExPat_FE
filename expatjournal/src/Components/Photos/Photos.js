41d3945630b24a5deeed303f9bf34e495b652771
jrizza88 <jetorres88@gmail.com>
2019-08-11 23:27:46 -0400

    initialized photo set up

---
+++ expatjournal/src/Components/Photos/Photos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Photos extends React.Component {
+
+    render(){
+    return (
+        <>
+        Photos!
+        </>
+    )
+    }
+}
+
+export default Photos;
