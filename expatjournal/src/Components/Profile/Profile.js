c0e177ee139a1c028ab6d283dc6610c32d0085e0
jrizza88 <jetorres88@gmail.com>
2019-10-15 23:10:48 -0400

    downloaded react bootstrap package

--- expatjournal/src/Components/Profile/Profile.js
+++ expatjournal/src/Components/Profile/Profile.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 const Profile = props => {
   
 return (
     <>
+    <Container>
         <h1>Welcome back, {props.name}!</h1>
         <h2>You have {props.posts} posts!</h2>
-            
+    </Container>
     </>
 )
 };
